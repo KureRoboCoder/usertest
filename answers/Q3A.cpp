c7f23df322d3e791adee97f6f458a15df3bf42a4
e22-ykjy <e22-ykjy@gw.kure-nct.ac.jp>
2026-08-04 15:00:32 +0900

    Q3A

---
+++ answers/Q3A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+
+    int n;
+    cin >> n;
+
+    if (n >= 60) {
+        cout << "Yamatonare" << endl;
+    }else {
+        cout << "Notonare" << endl;
+    }
+
+    return 0;
+}
