d0dbd9a347e0c6f32cbb5a27effc565bb0317567
e22-ykjy <e22-ykjy@gw.kure-nct.ac.jp>
2026-08-04 15:13:48 +0900

    Q3C

---
+++ answers/Q3C.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+
+    char c;
+    cin >> c;
+
+    if (c >= 'a' && c <= 'z') {
+        cout << "GROUP" << ((c - 'a') % 7 + 1) << endl;
+    }else {
+        cout << "GROUP" << ((c - 'A' + 5) % 7 + 1) << endl;
+    }
+
+    return 0;
+}
