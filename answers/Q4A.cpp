192b9b4309b11e955dc85f732aa5d32528d5dc1e
e22-ykjy <e22-ykjy@gw.kure-nct.ac.jp>
2026-08-05 17:20:09 +0900

    Q4A

---
+++ answers/Q4A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+
+    int n;
+    cin >> n;
+
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        if (a % 7 == 0) {
+            count++;
+        }
+    }
+    cout << count << endl;
+}
