7e7046238f4787735171586a5841728ec9776b41
e22-ykjy <e22-ykjy@gw.kure-nct.ac.jp>
2026-08-05 17:27:42 +0900

    a

--- answers/Q4C.cpp
+++ answers/Q4C.cpp
@@ -12,7 +12,7 @@ int main(void) {
     for (int i = l; i <= r; i++) {
         for (int j = i; j <= r; j++) {
             for (int k = j; k <= r; k++) {
-                if ((i + j + k) % 3 == 0)   count++;
+                if ((i + j + k) % 3 == 0)   count++;//
             }
         }
     }
